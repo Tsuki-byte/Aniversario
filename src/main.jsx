6091a30a9a85de834c551f978b59c60234b81e30
José Carlos Millán de Cortés <josecarlosmillandecortes@Tsuki-4.local>
2026-09-20 18:05:44 +0200

    Fix routing para github pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import App from './App.jsx'
 import PuertaApp from './PuertaApp.jsx'
 import './index.css'
 
-const path = window.location.pathname;
-const isPuerta = path.includes('puerta');
+const search = window.location.search;
+const hash = window.location.hash;
+const pathname = window.location.pathname;
+
+const isPuerta = search.includes('puerta') || hash.includes('puerta') || pathname.includes('puerta');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
